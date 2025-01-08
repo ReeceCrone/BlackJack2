2d685e8f3aac202574f51a21f7150f200b42d514
Reece Crone <reece.crone@gmail.com>
2025-01-08 15:40:06 -0600

    sets the MainUI as the scene

---
+++ src/main/java/com/example/blackjack2/BlackJackApp.java
@@ -0,0 +1,22 @@
+package com.example.blackjack2;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BlackJackApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        MainUI root = new MainUI();
+        Scene scene = new Scene(root, 500, 500);
+        stage.setTitle("Blackjack");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
