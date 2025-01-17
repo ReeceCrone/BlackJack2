3b0796e88b3e30c984728fe15a132ad48dc811df
Reece Crone <reece.crone@gmail.com>
2025-01-17 12:42:40 -0600

    assigned all the classes to there appropiate spots in other classes

--- src/main/java/com/example/blackjack2/MainUI.java
+++ src/main/java/com/example/blackjack2/MainUI.java
@@ -4,6 +4,19 @@ import javafx.scene.layout.StackPane;
 
 public class MainUI extends StackPane {
     public MainUI() {
+        Model model = new Model();
+        InteractionModel iModel = new InteractionModel();
+        TableView tableView = new TableView();
+        Controller controller = new Controller();
 
+        model.addSubscriber(tableView);
+        iModel.addSubscriber(tableView);
+        tableView.setInteractionModel(iModel);
+        tableView.setModel(model);
+        tableView.setupEvents(controller);
+        controller.setModel(model);
+        controller.setIModel(iModel);
+
+        this.getChildren().add(tableView);
     }
 }
