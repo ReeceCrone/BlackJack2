ad4daeb2328a79d4cfbdba3ea0a64e5a0721a0b0
Reece Crone <reece.crone@gmail.com>
2025-01-17 12:12:49 -0600

    dealCard method implemented and a index to keep track of the current top card

--- src/main/java/com/example/blackjack2/Shoe.java
+++ src/main/java/com/example/blackjack2/Shoe.java
@@ -4,29 +4,40 @@ import java.util.ArrayList;
 
 public class Shoe {
 
+    private int topCardIndex;
+
     //Our shoe will be made out of 6 decks of cards
     private final int shoeDecks = 6;
 
     //The list of cards that will be our shoe
-    private ArrayList<Card> shoe;
+    private ArrayList<Card> cards;
 
     public Shoe() {
-        shoe = new ArrayList<Card>();
+        topCardIndex = 0;
+        cards = new ArrayList<Card>();
         for (int i = 0; i < shoeDecks; i++) {
             for (Card.Rank rank : Card.Rank.values()) {
                 for (Card.Suit suit : Card.Suit.values()) {
-                    shoe.add(new Card(rank, suit));
+                    cards.add(new Card(rank, suit));
                 }
             }
         }
     }
 
+    //deals the top card in the shoe and moves the topCardIndex ahead one
+    public Card dealCard() {
+        int index = topCardIndex;
+        topCardIndex++;
+        return cards.get(index);
+    }
+
+    //shuffles the cards in the shoe
     public void shuffleShoe() {
         for (int i = 0; i < (shoeDecks * 52); i++) {
-            int randomIndex = (int) (Math.random() * shoe.size());
-            Card temp = shoe.get(0);
-            shoe.set(0, shoe.get(randomIndex));
-            shoe.set(randomIndex, temp);
+            int randomIndex = (int) (Math.random() * cards.size());
+            Card temp = cards.get(0);
+            cards.set(0, cards.get(randomIndex));
+            cards.set(randomIndex, temp);
         }
     }
 }
